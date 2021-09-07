b92dc585edce29283d895cf8d81e229032371222
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-08 02:00:54 +0900

    Feat: Get snippet information

---
+++ constants/messages.js
@@ -0,0 +1,8 @@
+const messages = {
+  OK: "ok",
+  INVALID_ID: "요청하신 리소스의 ID가 잘못되었습니다.",
+  INVALID_REQUEST: "잘못된 요청을 입력했습니다.",
+  INTERNAL_SERVER_ERROR: "서버상의 이유로 요청하신 작업을 수행할 수 없습니다.",
+};
+
+module.exports = messages;
