fea62e27a91335e80171ec2310c5960d19e65496
seabluemaker <seabluemaker@gmail.com>
2021-09-07 21:30:44 +0900

    Feat: Get snippet list

--- routes/snippets.js
+++ routes/snippets.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const Snippet = require("../models/Snippet");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const snippetList = await Snippet.find({ language: req.query.language });
+
+    const result = {
+      result: "ok",
+      snippetList,
+    };
+
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
