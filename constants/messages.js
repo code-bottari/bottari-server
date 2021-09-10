aa629aeede5a53b6448b1cfbc432ced5e5eea533
HannaChoi <hn4880@gmail.com>
2021-09-10 13:02:39 +0900

    Feat: Register user information
      - get idToken and verify
      - save user information
      - generate token and send to client

---
+++ constants/messages.js
@@ -0,0 +1,9 @@
+const MESSAGES = {
+  UNEXPECTED_ERROR: "서버상의 이유로 요청하신 작업을 수행할 수 없습니다.",
+  EXPIRED_TOKEN: "토큰이 만료되었습니다.",
+  INVALID_REQUEST: "유효하지 않은 요청입니다.",
+  INVALID_TOKEN: "유효하지 않은 토큰입니다.",
+  OK: "ok",
+};
+
+module.exports = MESSAGES;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,82 @@
 const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+
+const admin = require("firebase-admin");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
 
+const MESSAGES = require("../constants/messages");
+const ERRORS = require("../constants/errors");
+
+const {
+  INVALID_REQUEST,
+  EXPIRED_TOKEN,
+  UNEXPECTED_ERROR,
+  OK,
+} = MESSAGES;
+
+const {
+  AUTH_ID_TOKEN_REVOKED,
+  AUTH_ID_TOKEN_EXPIRED,
+} = ERRORS;
+
 router.post("/register", async (req, res, next) => {
+  const { idToken, nickname, imageUrl } = req.body;
+
   try {
-    const { nickname } = req.body;
+    if (idToken === undefined || nickname === undefined || imageUrl === undefined) {
+      throw createError(422, INVALID_REQUEST);
+    }
 
-    await User.create({ email: "이메일", nickname, imageUrl: "url"});
+    const decodedToken = await admin
+      .auth()
+      .verifyIdToken(idToken);
 
-    res.send({ result: "ok" });
+    if (!decodedToken) {
+      throw createError(403, EXPIRED_TOKEN);
+    }
+
+    const { email } = decodedToken;
+
+    const user = await User.create({
+      email,
+      nickname,
+      imageUrl,
+    });
+
+    const token = jwt.sign(user._id, process.env.SECRET_KEY);
+
+    res
+      .cookie("auth", token, {
+        maxAge: 1000 * 60 * 60, // hour = milliseconds * seconds * minutes
+        httpOnly: true,
+      })
+      .send({ result: OK });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error.ValidationError) {
+      next(createError(422, INVALID_REQUEST));
+
+      return;
+    }
+
+    const { code } = error;
+
+    if (code === AUTH_ID_TOKEN_REVOKED) {
+      next(createError(422, INVALID_TOKEN));
+
+      return;
+    }
+
+    if (code === AUTH_ID_TOKEN_EXPIRED) {
+      next(createError(401, EXPIRED_TOKEN));
+
+      return;
+    }
+
+    next({ message: UNEXPECTED_ERROR });
   }
 });
 
