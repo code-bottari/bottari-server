8029d3068fc7686364d3435f2757ce550bdeeb82
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-10 22:00:08 +0900

    Feat: Add middleware verifying token

---
+++ routes/middlewares/verifyToken.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+const {
+  EXPIRED_TOKEN,
+  INVALID_TOKEN,
+} = require("../../constants/messages");
+
+const {
+  TOKEN_EXPIRED_ERROR,
+  JSON_WEB_TOKEN_ERROR,
+} = require("../../constants/errors");
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.auth;
+
+  if (token === null) {
+    next(createError(401, INVALID_TOKEN));
+
+    return;
+  }
+
+  try {
+    jwt.verify(token, process.env.SECRET_KEY);
+
+  } catch (error) {
+    const { name } = error;
+
+    if (name === TOKEN_EXPIRED_ERROR) {
+      next(createError(401, EXPIRED_TOKEN));
+
+      return;
+    }
+
+    if (name === JSON_WEB_TOKEN_ERROR) {
+      next(createError(401, INVALID_TOKEN));
+
+      return;
+    }
+  }
+
+  next();
+};
+
+module.exports = { verifyToken };
