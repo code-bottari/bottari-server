04aad03475ab6e69ac5912ba765da447dd2f7ea2
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-11 12:22:33 +0900

    Feat: delete snippet

--- constants/messages.js
+++ constants/messages.js
@@ -1,10 +1,11 @@
 const MESSAGES = {
   INVALID_ID: "요청하신 리소스의 ID가 잘못되었습니다.",
+  NO_AUTHORITY_TO_ACCESS: "요청하신 작업을 수행할 권한이 없습니다.",
   UNEXPECTED_ERROR: "서버상의 이유로 요청하신 작업을 수행할 수 없습니다.",
   INVALID_REQUEST: "유효하지 않은 요청입니다.",
   INVALID_TOKEN: "유효하지 않은 토큰입니다.",
   EXPIRED_TOKEN: "토큰이 만료되었습니다.",
-  NOT_FOUND: "요청하신 정보를 찾을 수 없습니다.",
+  NOT_FOUND: "요청하신 데이터를 찾을 수 없습니다.",
   OK: "ok",
 };
 
