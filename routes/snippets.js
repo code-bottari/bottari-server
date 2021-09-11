e768ccc991cee20d28b319988354d5468db398ae
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-11 12:22:33 +0900

    Feat: delete snippet

--- routes/snippets.js
+++ routes/snippets.js
@@ -3,13 +3,18 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
 
 const Snippet = require("../models/Snippet");
+const Hashtag = require("../models/Hashtag");
 
 const {
   OK,
   INVALID_ID,
   INVALID_REQUEST,
+  NO_AUTHORITY_TO_ACCESS,
+  NOT_FOUND,
+  UNEXPECTED_ERROR,
 } = require("../constants/messages");
 
 router.get("/", async (req, res, next) => {
@@ -51,6 +56,58 @@ router.get("/:id", async (req, res, next) => {
     }
 
     next({ message: INTERNAL_SERVER_ERROR });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id: snippetId } = req.params;
+
+  const { auth: token } = req.cookies;
+
+  const { _id: userId } = jwt.decode(token);
+
+  try {
+    const snippet = await Snippet.findById(snippetId);
+
+    if (snippet === null) {
+      throw createError(404, NOT_FOUND);
+    }
+
+    const { poster, hashtagList } = snippet;
+
+    if (poster !== userId) {
+      throw createError(403, NO_AUTHORITY_TO_ACCESS);
+    }
+
+    await Snippet.findByIdAndDelete(snippetId);
+
+    const findAndDeleteHashtag = async (hashtag) => {
+      const currentHashtag = await Hashtag.findOne({ name: hashtag });
+
+      if (currentHashtag === null) {
+        return;
+      }
+
+      const index = currentHashtag.snippetList.findIndex((snippet) => String(snippet) === String(snippetId));
+
+      if (index === -1) {
+        return;
+      }
+
+      await currentHashtag.save();
+    };
+
+    await Promise.all(hashtagList.map(async (hashtag) => await findAndDeleteHashtag(hashtag)));
+
+    res.send({ result: OK });
+  } catch (error) {
+    if (error.status) {
+      next(error);
+
+      return;
+    }
+
+    next({ message: UNEXPECTED_ERROR });
   }
 });
 
