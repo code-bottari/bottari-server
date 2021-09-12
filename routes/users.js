72528cc4d6f71c6953856643d1eb3b070c90624d
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-12 21:18:50 +0900

    Refactor: Clean up unused codes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
-const mongoose = require("mongoose");
 const admin = require("firebase-admin");
 const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
