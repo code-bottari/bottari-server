cb63e763910519526a28a33b940e502158aaba2a
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-13 00:31:43 +0900

    Style: Modify code style

--- config/socketIo.js
+++ config/socketIo.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 const connectSocketIo = (app) => {
-  app.io.on("connection", function (socket) {
+  app.io.on("connection", (socket) => {
     console.log("connected");
 
-    socket.on("disconnect", function () {
+    socket.on("disconnect", () => {
       console.log("disconnected");
     });
   });
