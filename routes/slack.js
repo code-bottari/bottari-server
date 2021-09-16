21effd1a86764be5dfa8bc1c1438029cd93955e6
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-09-17 01:06:14 +0900

    Feat: Add slack access token management

--- routes/slack.js
+++ routes/slack.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
+const SlackToken = require("../models/SlackToken");
+
 const app = require("../config/connectSlack");
-const { SLACK_BOT_CLIENT_ID, SLACK_BOT_CLIENT_SECRET } = require("../config/envConfig");
+
+const {
+  SLACK_BOT_CLIENT_ID,
+  SLACK_BOT_CLIENT_SECRET,
+} = require("../config/envConfig");
 
 router.get("/authorize", async (req, res, next) => {
   const { code } = req.query;
@@ -11,6 +18,11 @@ router.get("/authorize", async (req, res, next) => {
     client_secret: SLACK_BOT_CLIENT_SECRET,
     code,
   });
+
+  const { team, access_token: accessToken } = response;
+  const { id: teamId } = team;
+
+  await SlackToken.create({ teamId, accessToken });
 
   res.send(response);
 });
