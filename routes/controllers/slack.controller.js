b4167b0cb91af29491c0fbab28c73f559af54fdc
Hwan Choi <hwanchoi@Hwanui-MacBookPro.local>
2021-11-11 02:39:13 +0900

    Refactor: Change redirect url in authorizeSlackApp

--- routes/controllers/slack.controller.js
+++ routes/controllers/slack.controller.js
@@ -5,23 +5,28 @@ const { app } = require("../../config/connectSlack");
 const {
   SLACK_BOT_CLIENT_ID,
   SLACK_BOT_CLIENT_SECRET,
+  CLIENT_URL,
 } = require("../../config/envConfig");
 
 const authorizeSlackApp = async (req, res, next) => {
   const { code } = req.query;
 
-  const response = await app.client.oauth.v2.access({
-    client_id: SLACK_BOT_CLIENT_ID,
-    client_secret: SLACK_BOT_CLIENT_SECRET,
-    code,
-  });
+  try {
+    const response = await app.client.oauth.v2.access({
+      client_id: SLACK_BOT_CLIENT_ID,
+      client_secret: SLACK_BOT_CLIENT_SECRET,
+      code,
+    });
 
-  const { team, access_token: accessToken } = response;
-  const { id: teamId } = team;
+    const { team, access_token: accessToken } = response;
+    const { id: teamId } = team;
 
-  await SlackToken.create({ teamId, accessToken });
+    await SlackToken.create({ teamId, accessToken });
 
-  res.send(response);
+    res.redirect(`${CLIENT_URL}/slack/greeting`);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = authorizeSlackApp;
